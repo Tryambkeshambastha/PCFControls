5a1452b1d4f8fa776ea14fe68d6fbe281a44b0d7
Tryambkeshambastha <tryambkeshambastha.official@gmail.com>
2026-07-11 13:15:23 +0530

    Delete Scheduler/Scheduler/components/callbacks/index.ts

--- Scheduler/Scheduler/components/callbacks/index.ts
+++
@@ -1,7 +0,0 @@
-export {createEventClickedCallback} from "./eventClicked";
-export {createSlotClickedFuncCallback} from "./slotClickedFunc";
-export {createNewEventCallback} from "./newEvent";
-export {createToggleExpandFuncCallback} from "./toggleExpandFunc";
-export {createNextClickCallback} from "./nextClick";
-export {createPrevClickCallback} from "./prevClick";
-export {createOnViewChangeCallback} from "./onViewChange";
